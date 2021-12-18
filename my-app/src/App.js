a03b4e56a596bfda5d6bf6d08cf38bd4863ada33
ngoclong-fulbright <69509154+ngoclong-fulbright@users.noreply.github.com>
2021-12-18 15:57:53 +0700

    add react route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {Component} from 'react';
-import {Image, Button, Row} from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import { Navigate, BrowserRouter, Routes, Route } from 'react-router-dom'
+import WelcomePage from './WelcomePage';
+
+
 function App() {
   return (
-    <div className="App">
-      <div className="decor" >
-      <Image src="/image/Group6.png" alt='decor'/>
-      </div>
-      <div class="container">
-      <Row>
-        <div className='mt-5 mb-5'>
-        <Image src='/image/logo_Fulbright.svg' alt='logo'/>
-        </div>
-      </Row>
-      <Row >
-        <div >
-          <Button className="mb-3 logSignButton">Login</Button>{' '}
-        </div>
-      </Row>
-      <Row >
-        <div >
-        <Button className="logSignButton">Sign up</Button>{' '}
-        </div>
-      </Row>
-      </div>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/welcome' element={<WelcomePage />}/>
+        <Route path="/" element={<Navigate replace to="/welcome" />} />
+        {/* <Route path='/restricted'>
+        <Restricted />
+        </Route>       */}
+      </Routes>
+    </BrowserRouter>
     
   );
 }
