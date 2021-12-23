ee8afb8cc4d9d36d0e3283b65dc83f224765afb0
ngoclong-fulbright <69509154+ngoclong-fulbright@users.noreply.github.com>
2021-12-24 00:23:28 +0700

    http.cors() inner method

--- spring-backend/src/main/java/com/qnaforum/webapp/SecurityConfig.java
+++ spring-backend/src/main/java/com/qnaforum/webapp/SecurityConfig.java
@@ -2,19 +2,35 @@ package com.qnaforum.webapp;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
+import java.util.List;
+
+/*@Configuration*/
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    /*https://stackoverflow.com/questions/32319396/cors-with-spring-boot-and-angularjs-not-working*/
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
-                .cors()
-                .and()
-                .antMatcher("/**").authorizeRequests()
-                .antMatchers(new String[]{"/", "/welcome", "/not-restricted"}).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .oauth2Login();
+            .cors().configurationSource(request -> {
+                var cors = new CorsConfiguration();
+                cors.setAllowedOrigins(List.of("*"));
+                cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
+                cors.setAllowedHeaders(List.of("*"));
+                cors.setAllowedOriginPatterns(List.of("*"));
+                return cors;
+            })
+            .and()
+            .antMatcher("/**").authorizeRequests()
+            .antMatchers(new String[]{"/", "/welcome", "/not-restricted"}).permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .oauth2Login();
     }
 }
--- spring-backend/src/main/java/com/qnaforum/webapp/WebAppController.java
+++ spring-backend/src/main/java/com/qnaforum/webapp/WebAppController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 /*@RequestMapping("/")*/
-@CrossOrigin(origins = "*")
+/*@CrossOrigin(origins = "*")*/
 public class WebAppController {
     @GetMapping("/welcome")
     public String helloWorld() {
