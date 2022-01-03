c78b11f557b018d4382ea4af6fa9707ce71b6ba3
NguyetQue1211 <72976388+Lauraphan@users.noreply.github.com>
2022-01-03 22:30:03 +0700

    AddingBaseCode

---
+++ spring-backend/src/main/java/FuvSoftware/QandAForum/repository/UserRepository.java
@@ -0,0 +1,33 @@
+package FuvSoftware.QandAForum.repository;
+
+
+import FuvSoftware.QandAForum.exception.ResourceNotFoundException;
+import FuvSoftware.QandAForum.model.user.User;
+import FuvSoftware.QandAForum.security.UserPrincipal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(@NotBlank String username);
+
+    Optional<User> findByEmail(@NotBlank String email);
+
+    Boolean existsByUsername(@NotBlank String username);
+
+    Boolean existsByEmail(@NotBlank String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    default User getUser(UserPrincipal currentUser) {
+        return getUserByName(currentUser.getUsername());
+    }
+
+    default User getUserByName(String username) {
+        return findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+    }
+}
