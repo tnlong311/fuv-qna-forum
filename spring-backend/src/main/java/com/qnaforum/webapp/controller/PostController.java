79f68e870c3826e12c5bdd435e119d0faba70e6c
NguyetQue1211 <que.phan.200168@student.fulbright.edu.vn>
2022-01-04 14:26:25 +0700

    Create PostController.java

---
+++ spring-backend/src/main/java/com/qnaforum/webapp/controller/PostController.java
@@ -0,0 +1,12 @@
+package com.qnaforum.webapp.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class PostController {
+
+
+}
