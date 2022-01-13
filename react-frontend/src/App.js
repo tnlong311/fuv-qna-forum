661ed7e2d4e9ead403ecc799ce2d9cd120e2ba39
longtran292 <tranvannhatlong292@gmail.com>
2022-01-13 13:07:52 +0700

    Update frontend

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,21 +1,74 @@
 import './App.css';
-import React from 'react';
-import { Navigate, BrowserRouter, Routes, Route } from 'react-router-dom'
+import React , { useState, useEffect }from 'react';
+import { Navigate, BrowserRouter, Routes,Route, Switch, Router, NavLink } from 'react-router-dom'
 import WelcomePage from './WelcomePage';
 import Restricted from "./Restricted";
-
+import axios from 'axios';
+import Login from './Login';
+import Signup from './Signup';
+import Dashboard from './Dashboard';
+import Home from './Home';
+import PrivateRoute from './Utils/PrivateRoute';
+import PublicRoute from './Utils/PublicRoute';
+import { getToken, removeUserSession, setUserSession } from './Utils/Common';
 
 function App() {
+  const [authLoading, setAuthLoading] = useState(true);
+  var config = {
+    headers: {
+      "Content-Type": 'application/json',
+    },
+  };
+  useEffect(() => {
+    const token = getToken();
+    if (!token) {
+      return;
+    }
+    axios.get('http://localhost:8080/api/test',{token},config).then(response => {
+      setUserSession(response.data.token, response.data.user);
+      setAuthLoading(false);
+    }).catch(error => {
+      removeUserSession();
+      setAuthLoading(false);
+    });
+  }, []);
+
+  if (authLoading && getToken()) {
+    return <div className="content">Checking Authentication...</div>
+  }
   return (
-    <BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
       <Routes>
-        <Route path='/welcome' element={<WelcomePage />}/>
-        <Route path="/" element={<Navigate replace to="/welcome" />} />
-        <Route path='/restricted' element={<Restricted />}/>
+        <Route exact path="/" element={<WelcomePage />}/>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
-    </BrowserRouter>
-    
+      </BrowserRouter>
+      
+    </div>    
   );
 }
+{/* <div className="App">
+      <BrowserRouter>
+        <div>
+          <div className="header">
+            <NavLink exact activeClassName="active" to="/">Home</NavLink>
+            <NavLink activeClassName="active" to="/login">Login</NavLink><small>(Access without token only)</small>
+            <NavLink activeClassName="active" to="/dashboard">Dashboard</NavLink><small>(Access with token only)</small>
+          </div>
+          <div className="content">
+            <Routes>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/dashboard" component={Dashboard} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+} */}
 
 export default App;
